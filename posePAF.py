682ee22b07dacaba05648f0981ea6a2c4cbe5dc9
Ben Roye <benjamin.reed.roye@gmail.com>
2018-07-13 00:28:40 +0200

    Beginning of crowd mask creation.

--- posePAF.py
+++ posePAF.py
@@ -1,13 +1,12 @@
 import os
 import sys
 import cv2
-# import skimage.io as io
 import numpy as np
 import argparse as ap
-# import matplotlib.pyplot as plt
 # import cnnArchs
-sys.path.append('./pose/cocoapi/PythonAPI/')
+from builtins import FileExistsError
 from pycocotools.coco import COCO
+import pycocotools.mask as msk
 import matplotlib.pyplot as plt
 
 
@@ -34,36 +33,80 @@ def getMeanAndStdOfImgDir(path_to_dir):
     return np.append(means, std)
 
 
-def test(dataDir=''):
+def test(dataDir='', dataType='train2014'):
     if not dataDir:
         dataDir = os.path.dirname(os.path.realpath(sys.argv[0]))
-    dataType = 'train2014'
     anFile = '{}/annotations/person_keypoints_{}.json'.format(dataDir, dataType)
     cc = COCO(anFile)
     catIds = cc.getCatIds(catNms=['person'])
     if (len(catIds) == 0):
+        print('No files with "person" category in annotations.')
         return
     imgIds = cc.getImgIds(catIds=catIds)
     anns = cc.loadAnns(cc.getAnnIds(imgIds=imgIds))
     anns = [i for i in anns if 'keypoints' in i]
     if (len(anns) == 0):
+        print('No annotations contain keypoints.')
         return
-    # print(anns[0])
     img_fn = 'COCO_{}_{:012d}.jpg'.format(dataType, anns[0]['image_id'])
     imgIds = anns[0]['image_id']
     anns = cc.loadAnns(cc.getAnnIds(imgIds=imgIds))
+    for ann in anns:
+        print('num_keypoints: {}'.format(ann['num_keypoints']) +
+              '  iscrowd: {}'.format(ann['iscrowd']))
     fullpath = '{}{}/{}'.format(dataDir, dataType, img_fn)
     img = cv2.imread(fullpath)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.figure()
     plt.imshow(img)
     plt.axis('off')
     cc.showAnns(anns)
     plt.show()
 
 
+def genCrowdMasks(dataDir, dataType='train2017'):
+    if not dataDir:
+        dataDir = dataDir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    maskDir = os.path.join(dataDir, 'masks/', dataType, '')
+    try:
+        os.makedirs(maskDir)
+    except FileExistsError:
+        print('masks directory already exists')
+    anFile = '{}/annotations/person_keypoints_{}.json'.format(dataDir, dataType)
+    cc = COCO(anFile)
+    img_ids = cc.getImgIds(catIds=cc.getCatIds(['person']))
+    green = tuple(map(int, np.uint8(np.array([0, 255, 0]))))
+    for i in range(1):
+        anns = cc.loadAnns(cc.getAnnIds(imgIds=img_ids[i]))
+        img = cc.loadImgs(img_ids[i])[0]
+        falseMask = np.zeros((img['height'], img['width'], 3), dtype=np.uint8)
+        # falseMask = np.asfortranarray(falseMask)
+        # crowdMask = msk.encode(falseMask)
+        print('{}:image {} has {} annotations'.format(i, img_ids[i], len(anns)))
+        for ann in anns:
+            if (ann['iscrowd']):
+                mask = msk.frPyObjects([ann['segmentation']],
+                                       img['height'], img['width'])
+                mask = msk.decode(mask)
+                mask = mask * np.array([255, 0, 0], dtype=np.uint8)
+                falseMask = np.maximum(falseMask, mask)
+            elif (ann['num_keypoints'] == 0):
+                buf = np.zeros((img['height'], img['width'], 3), dtype=np.uint8)
+                try:
+                    pts = np.reshape(ann['segmentation'][0], (-1, 2))
+                except KeyError:
+                    print(ann['segmentation'])
+                pts = pts.astype(int)
+                cv2.fillPoly(buf, [pts], green)
+                np.maximum(falseMask, buf, out=falseMask)
+        plt.imshow(falseMask)
+        # plt.axis('off')
+    plt.show()
+
+
 def main():
     parser = ap.ArgumentParser(description='2D Pose Detection w/ PyTorch')
-    parser.add_argument('-dir', metavar='DIR', help='path to dataset')
+    parser.add_argument('-d', '--data', dest='data_dir', help='path to dataset')
     parser.add_argument('-t', '--test', dest='test_dir',
                         help='directory for annotation testing')
     args = parser.parse_args()
@@ -75,6 +118,9 @@ def main():
         except AssertionError:
             print('Test images did not load properly. Check given directory ' +
                   'is correct. It should end with a slash.')
+    if (args.data_dir is not None):
+        genCrowdMasks(args.data_dir)
+
 
 if __name__ == '__main__':
     main()
